1638d88cc09abc5679ca5d7fd0abf8cb3a4d31e0
sumanth82 <sumanth82@gmail.com>
2020-07-13 23:21:21 -0400

    Updates

--- sec-6-OOP/inheritance2.py
+++ sec-6-OOP/inheritance2.py
@@ -2,15 +2,19 @@
 # ANS: isinstance - A built-in function 
 # isinstance(instance, class)
 
+# Everything in Python inherits from the - object base class.
+
+# So the below class User(), under the hood takes - class User(object) as it's parent class. 
 
 class User():
     def sign_in(self):
         print('logged in')
 
 
-user_signin = User('Merlin')
-
+user_signin = User()
 user_signin.sign_in()
 
-print(isinstance(user_signin, User))
+print(isinstance(user_signin, User))    # O/P: True
+
+
 
