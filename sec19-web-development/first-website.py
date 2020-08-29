a45bea9ecc250fde29a5f325dcb7a4f2db82029a
sumanth82 <sumanth82@gmail.com>
2020-08-29 16:38:33 -0400

    Updates

---
+++ sec19-web-development/first-website.py
@@ -0,0 +1,14 @@
+# web-server folder contains html (for information/document), js (for interaction), css (for styling, font)
+# web-server simply gives the 3 files to thr browser to display the page - html, js, css
+
+# In the html file you need to add the <link> tag to refer the css file in the website/html display page
+#href - says where is the file located
+
+# <link type="text/css" href="style.css">
+
+# To ref a js file in the html file, add it under <body>
+
+# <script>src='script.js'</script>
+
+
+
