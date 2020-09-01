738156867ce66461829c88a633d68f2f1530ee75
sumanth82 <sumanth82@gmail.com>
2020-09-01 08:07:35 -0400

    Updates

--- sec19-web-development/building-a-portfolio.py
+++ sec19-web-development/building-a-portfolio.py
@@ -22,14 +22,101 @@
 # export FLASK_APP=portfolio.py
 # flask run
 
-
 # Free templates websites:
-
 # https://www.creative-tim.com/bootstrap-themes/ui-kit?direction=asc&sort=price
-
 # https://html5up.net/
 
+# HOW to GENERATE THE ROUTES dynamically instead of to specifying for each end-point?
+
+# Answer : See below
+
+#Ex:
+
+#@app.route('/string:page_name)')
+
+#def html_page(page_name):
+#    return render_template(page_name)
+
+#####
+
+## How to SUBMIT a FORM! 
+
+# You use the request object in flask
+
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/#accessing-request-data
+
+
+#### Once you submit the contact us info or something, how to store that on the server?
+
+## Once you submit the data, to capture on the server the form, needs to have name entry. 
+
+# Mainly line 67-70
+
+'''
+
+ <input type="email" class="form-control" id="email" placeholder="Email">
+                      </div>
+                      <div class="form-group">
+                        <input type="text" class="form-control" id="subject" placeholder="Subject">
+
+'''
+
+## After the data is submitted by user, rxed by the server, send a response/acknowledgement to thank you
+
+#This is done using the redirect() module
+
+
+'''
+
+@app.route('/submit_form', methods=['POST', 'GET']) # For this to work, make some changes to the contact.html under <send> tab# See changes in line 75 and 62
+#def submit_form():
+#    return 'Form Submitted! Thank you!'
+def get_data():                                  # Use this function to get the contact data posted by the e-mail client
+    if request.method == 'POST':
+        data=request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+
+'''
+
+## Storing the received data in persistent data store 
+
+### TO grab and store the data in the backend, in the html file, you need to add name= parameter for the submit form fields
 # 
+#  Like below:
+
+'''
+
+<div class="form-group">
+                        <input name="email" type="email" class="form-control" id="email" placeholder="Email">
+                      </div>
+                      <div class="form-group">
+                        <input name="subject" type="text" class="form-control" id="subject" placeholder="Subject">
+                      </div>
+                      <div class="form-group">
+                        <textarea name="message" class="form-control" rows="5" placeholder="Enter your message"></textarea>
+                      </div>
+                      <button type="submit" class="btn btn-default btn-lg">Send</button>
+
+'''
+
+# After this, you will have a Form Data Field in the developer's tools '
+
+'''
+
+email: sumanth82@gmail.com
+subject: sdfds
+message: sdfds
+
+'''
+
+## To access this data you can use the request.forms in flask
+
+# building-a-portfolio-6
+
+# To store the data, create a file called - database.txt
+
+# building-a-portfolio-7 ; Instead of writing the file to txt. write it to csv
 
 
 
--- sec19-web-development/portfolio-project/portfolio.py
+++ sec19-web-development/portfolio-project/portfolio.py
@@ -1,30 +1,54 @@
 import flask
 
-from flask import url_for, Flask, render_template
+from flask import url_for, Flask, render_template, request, redirect
 app=Flask(__name__)
 
 @app.route('/')
-
 def my_home():
     return render_template('index.html')
 
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
+@app.route('/<string:page_name>')             # With this single entry, you can comment out the rest of the lines
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/work.html')
-def work():
-    return render_template('work.html')
 
-@app.route('/about.html')
-def about_us():
-    return render_template('about.html')
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:                                  # Create a function to store the data rxed in forms in database.txt
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file=database.write(f'\n {email}, {subject}, {message}')
 
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
 
-@app.route('/components.html')
-def component():
-    return render_template('components.html')
+@app.route('/submit_form', methods=['POST', 'GET']) # For this to work, make some changes to the contact.html under <send> tab# See changes in line 75 and 62
+#def submit_form():
+#    return 'Form Submitted! Thank you!'
+def get_data():                                  # Use this function to get the contact data posted by the e-mail client
+    if request.method == 'POST':
+        data=request.form.to_dict()
+        write_to_file(data)                     # Call this function to store the data in database.txt
+        print(data)
+        return redirect('/thankyou.html')       # import redirect method for this 
+    else:
+        return 'something went wrong'
 
+
+#@app.route('/works.html')
+#def works():
+#    return render_template('works.html')
+
+#@app.route('/work.html')
+#def work():
+#    return render_template('work.html')
+
+#@app.route('/about.html')
+#def about_us():
+#    return render_template('about.html')
+
+#@app.route('/contact.html')
+#def contact():
+#    return render_template('contact.html')
+
+#@app.route('/components.html')
+#def component():
+#    return render_template('components.html')
