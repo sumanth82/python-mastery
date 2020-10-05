ef93c4fba01ae0ced64f46714bffa6e1a6620d2e
sumanth82 <sumanth82@gmail.com>
2020-10-05 06:04:31 -0400

    updates

---
+++ sec-2-python-intro/interpreter.py
@@ -0,0 +1,12 @@
+'''
+1. You have Python Code -> A High Level Code; 
+2. Run it using an interpreter (code is executed one line at a time unlike compiler)--> Converts this to a byte code 
+(Identical to Machine level code)
+
+ex:
+0 LOAD GLOBAL 
+2 LOAD CONST. 
+
+3. This Byte code is then converted to a Machine level code in a - cypython VM
+
+'''
