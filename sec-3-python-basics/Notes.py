6d9ad2429e90fed87274d03f82581de435405f63
sumanth82 <sumanth82@gmail.com>
2020-10-08 06:03:07 -0400

    Updates

--- sec-3-python-basics/Notes.py
+++ sec-3-python-basics/Notes.py
@@ -6,4 +6,12 @@
 print(int('0b101', 2))  # O/P: 5
 
 
+#### VARIABLES in Python #####
+
+_user_iq=190  # This denotes a PRIVATE Variable in Python i.e a varibale which starts with a _ ;
+
+
+
+
+
 
---
+++ sec-3-python-basics/variables.py
@@ -0,0 +1,17 @@
+
+print(int('0b101', 2))  # O/P: 5
+
+#### VARIABLES in Python #####
+
+_user_iq=190  # This denotes a PRIVATE Variable in Python i.e a varibale which starts with a _ ;
+
+PI = 3.14    # Variables defined in UPPERCASE denotes a CONSTANT and MUST NOT BE CHANGED ideally; 
+
+__name__ == '__main__'      # variables that being with a double underscore __ are called dunder variables and shud be reserved;
+
+a,b,c=1,2,3
+
+print(a)
+print(b)
+print(c)
+
