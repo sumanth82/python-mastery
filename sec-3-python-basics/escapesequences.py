1930a44db80380bb913a1ea062c8f94158fb09e5
sumanth82 <sumanth82@gmail.com>
2020-10-09 05:58:50 -0400

    Updates

---
+++ sec-3-python-basics/escapesequences.py
@@ -0,0 +1,11 @@
+weather = 'It's kind of sunny'
+
+weather = "It's kind of sunny"
+
+weather = 'it\'s \'kind of sunny\''
+
+# Whatever comes after a \ is a string; So \ is the escape character
+# 
+
+weather = '\t it\'s kind of sunny here \n Hope you have a good day'
+
