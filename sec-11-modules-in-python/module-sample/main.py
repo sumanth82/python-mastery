8a3f5ab1ba128dad65b6297aa37ac1cd73070a7a
sumanth82 <sumanth82@gmail.com>
2020-11-19 04:31:56 -0500

    Updates

---
+++ sec-11-modules-in-python/module-sample/main.py
@@ -0,0 +1,7 @@
+import utility
+
+print(utility)
+print(utility.multiply(2, 3))
+
+# O/P: <module 'utility' from '/Users/sumantrenukarya/SUMANT/TECH/Python-Projects/python-mastery/sec-11-modules-in-python/module-sample/utility.py'>
+# 6
---
+++ sec-11-modules-in-python/modules.py
@@ -0,0 +1,13 @@
+# A module is a collection of py files
+# Each .py file is a module - each can have classes, functions
+# Essentially helps to organize code
+# module naming convention - snake_case
+
+# One of the common ways to utilize a module is using utility - simple functions that can be used all across the projects:
+# structure:
+
+# /module
+#   - main.py           - import utility
+#   - utility.py        - Define common functions
+
+# NOTE: When you import a module, it generates a __pycache folder; it caches the imported utility to load compiled utility
