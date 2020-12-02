31f6d9208597c4349a1a57db22525ac82cb6f4ad
sumanth82 <sumanth82@gmail.com>
2020-12-02 06:50:13 -0500

    updates

---
+++ sec-18-scraping-data-with-python/webscraping-basics.py
@@ -0,0 +1,9 @@
+# Download webpage (js, html, css), grab the data from web page and removing/parse the data that we don't need/need;
+
+# TO SEE WHAT DATA IS ALLOWED TO BE SCRAPED IN ANY WEBSITE, GO TO THE WEBSITE WITH A /robot.txt
+
+# https: // www.airbnb.com/robots.txt
+
+# In order to access a website endpoint using an API, you need a JSON based token to authenticate
+
+https: // www.udemy.com/api-2.0/courses/
