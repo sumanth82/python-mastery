8056fc9769dba5265598aabb480a20fddeaaf9c0
sumanth82 <sumanth82@gmail.com>
2020-12-02 06:55:00 -0500

    updates

---
+++ sec-18-scraping-data-with-python/How-googlebot-works.py
@@ -0,0 +1,1 @@
+# googlebot - google uses index to search all the webpages (SEO)
--- sec-18-scraping-data-with-python/webscraping-basics.py
+++ sec-18-scraping-data-with-python/webscraping-basics.py
@@ -7,3 +7,5 @@
 # In order to access a website endpoint using an API, you need a JSON based token to authenticate
 
 https: // www.udemy.com/api-2.0/courses/
+
+# If there is no robots.txt for a website, it means the website says to google rank my website, scrape all the data - I don't care
